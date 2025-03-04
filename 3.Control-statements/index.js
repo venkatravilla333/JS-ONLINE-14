7758a8ec9edbe802b5fe6122ce43d06f2f0216ee
venkat ravilla <venkatravilla333@gmail.com>
2025-03-04 22:22:28 +0530

    loops

--- 3.Control-statements/index.js
+++ 3.Control-statements/index.js
@@ -18,21 +18,102 @@
 
 // switch case
 
-var marks = 70
+// var marks = 70
 
-switch (marks) {
-  case marks < 35:
-    console.log('fail')
-    break;
-  case marks >= 35 && marks < 60:
-    console.log('second class')
-    break;
-  case marks >= 60 && marks < 75:
-    console.log('first class')
-    break;
-  case marks >= 75 && marks <= 100:
-    console.log('distinction')
-    break;
-  default:
-    console.log('Invalid marks')
-}
+// switch (marks) {
+//   case marks < 35:
+//     console.log('fail')
+//     break;
+//   case marks >= 35 && marks < 60:
+//     console.log('second class')
+//     break;
+//   case marks >= 60 && marks < 75:
+//     console.log('first class')
+//     break;
+//   case marks >= 75 && marks <= 100:
+//     console.log('distinction')
+//     break;
+//   default:
+//     console.log('Invalid marks')
+// }
+
+
+
+// var weekDay = 8
+
+// if (weekDay === 1) {
+//   console.log('Monday')
+// } else if (weekDay === 2) {
+  //   console.log('thesday')
+// } else if (weekDay === 3) {
+//   console.log('wednes day')
+// } else if (weekDay === 4) {
+//   console.log('thusday')
+// } else if (weekDay === 5) {
+//   console.log('Friday')
+// } else if (weekDay === 6) {
+  //   console.log('Saturday')
+// } else if(weekDay === 7) {
+  //   console.log('sunday day')
+  // } else {
+    //   console.log('invalid day')
+    // }
+    
+    
+// var weekDay = 1
+ 
+// switch (weekDay) {
+//   case 1:
+//     console.log('monday')
+//     break;
+//   case 2:
+//     console.log('thesday')
+//     break;
+//   case 3:
+//     console.log('wednes day')
+//     break;
+//   case 4:
+//     console.log('thusday')
+//     break;
+//   case 5:
+//     console.log('Friday')
+//     break;
+//   case 6:
+//     console.log('Saturday')
+//     break;
+//   case 7:
+//     console.log('sunday')
+//     break;
+//   default:
+//     console.log('invalid day')
+    
+// }
+
+
+// var customer = 'Gold'
+
+// if (customer === 'Gold') {
+//   console.log('Gold customer')
+// } else if (customer === 'Silver') {
+//   console.log('Silver customer')
+// } else if (customer === 'Iron') {
+//   console.log('iron custoer')
+// } else {
+//   console.log('Unknown customer')
+// }
+
+// var customer = 'mongo'
+
+// switch (customer) {
+//   case 'Gold':
+//     console.log('gold customer')
+//     break;
+//   case 'Silver':
+//     console.log('silver customer')
+//     break;
+//   case 'Iron':
+//     console.log('iron customer')
+//     break;
+//   default:
+//     console.log('Unknown customer')
+// }
---
+++ 4.Loops/index.js
@@ -0,0 +1,143 @@
+
+
+var data = 'sachin'
+
+//without using loops
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+// console.log(data)
+
+//with using loops
+
+//conditional loops
+
+// for loop
+
+// for (var i = 1; i <= 10; i++){
+//   console.log(i, data)
+// }
+
+//while loop
+
+// var i = 11
+
+// while (i<=10) {
+//   console.log(i, data)
+//   i++
+// }
+
+//do while loop
+
+// var i = 11
+
+// do {
+//   console.log(i, data)
+//   i++
+// }while(i<=10)
+
+//infinity loop
+
+// for (var i = 1; i > 0; i++){
+//   console.log(i, data)
+// }
+
+
+
