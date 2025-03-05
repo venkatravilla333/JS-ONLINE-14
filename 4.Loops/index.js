c31396f25893c817a3fce5f384837ba455304067
venkat ravilla <venkatravilla333@gmail.com>
2025-03-05 23:04:34 +0530

    for in & for of loop

--- 4.Loops/index.js
+++ 4.Loops/index.js
@@ -112,6 +112,7 @@ var data = 'sachin'
 // for loop
 
 // for (var i = 1; i <= 10; i++){
+//   if(i == 5) continue
 //   console.log(i, data)
 // }
 
@@ -138,6 +139,68 @@ var data = 'sachin'
 // for (var i = 1; i > 0; i++){
 //   console.log(i, data)
 // }
+
+
+// var person = {
+//   name: 'sachin',
+//   age: 40,
+//   city: 'mumbai',
+//   play() {
+//     console.log('Play')
+//   }
+// }
+
+// console.log(person)
+
+//without loop
+
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.city)
+// person.play()
+
+//with using loop
+
+// for in loop
+
+// for (var x in person) {
+//   console.log(x, person[x])
+// }
+// for (var x of person) {
+//   console.log(x, person[x])
+// }
+
+
+// var iterableDs = [1, 2, 3, 4, 5]
+// console.log(iterableDs)
+
+// var iteratorObj = iterableDs[Symbol.iterator]()
+// console.log(iteratorObj)
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+
+// for (var x of iterableDs) {
+//   console.log(x)
+// }
+
+// for (var x in iterableDs) {
+//   console.log(x, iterableDs[x])
+// }
+
+// with out using loop
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+// with using loop 
+
 
 
 
