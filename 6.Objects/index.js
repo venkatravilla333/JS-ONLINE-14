e4d29ded3b47a60b5d6009305c06cccbc6b9ec0e
venkat ravilla <venkatravilla333@gmail.com>
2025-03-12 22:41:01 +0530

    copy

--- 6.Objects/index.js
+++ 6.Objects/index.js
@@ -78,3 +78,135 @@
 
 // console.log(typeof Test) //
 
+//Objects Dynamic
+
+// let person = {
+//   name: 'sachin',
+//   age: 40
+// }
+
+//How to access object
+
+// console.log(person)
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person['name'])
+// console.log(person['age'])
+
+//How to add new properties to existing object
+
+// person.city = 'Mumbai'
+
+// person.play = function () {
+//   console.log('play')
+// }
+
+// console.log(person)
+
+//How to update existing object values
+
+// person.age = 45
+// console.log(person)
+
+//How to remove object properties
+
+// delete person.city
+// delete person.play
+// console.log(person)
+
+
+var person = {
+  name: 'sachin',
+  age: 40,
+  color: 'red',
+  city: 'Mumbai',
+  height: 5,
+  nums:[1,2,3,4,5],
+  play: () => {
+    console.log('Play')
+  }
+}
+
+//Without using loop
+
+// console.log(person)
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.color)
+// console.log(person.city)
+// person.play()
+
+//With using loop
+
+// for in
+
+// for (var key in person) {
+//   console.log(key, person[key])
+// }
+
+
+// let keys = Object.keys(person)
+// console.log(keys)
+
+// let values = Object.values(person)
+// console.log(values)
+
+// let entries = Object.entries(person)
+
+// console.log(entries)
+
+//Copy
+
+//primitives
+
+//Deep copy
+
+// var x = 10
+
+// var y = x
+
+// console.log(x)
+// console.log(y)
+// y = 20
+// console.log(x)
+// console.log(y)
+
+//Reference (object)
+
+// import lodash from 'lodash'
+
+// var lodash = require('lodash')
+
+var originalObj = {
+  name: 'sachin',
+  address: { road: 101 },
+  play: () => {
+    console.log('Play')
+  }
+}
+
+var copiedObj = originalObj // normal copy
+// var copiedObj = JSON.parse(JSON.stringify(originalObj)) // deep copy
+// var copiedObj = lodash.cloneDeep(originalObj) // deep copy
+// var copiedObj = Object.assign({}, originalObj) // shallow copy (ES-5)
+// var copiedObj = {...originalObj} // shallow copy (ES-6)
+
+// copiedObj.name = 'kohli'
+// copiedObj.address.road = 201
+
+// console.log(originalObj)
+// console.log(copiedObj)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
