a5c11ae0c275f6dc9e2f789eaa904a1069f65d18
venkat ravilla <venkatravilla333@gmail.com>
2025-03-18 22:35:34 +0530

    arrays

--- 8.Arrays/index.js
+++ 8.Arrays/index.js
@@ -94,6 +94,140 @@
 // var combineArr = [...arr1, ...arr2, ...arr3, ...x]
 // console.log(combineArr)
 
+//How to loop array values
+
+var arr = [1, 2, 3, 4, 5, 6]
+
+//without using loop
+
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
+
+//with using loop
+
+// for of loop
+
+// for (var value of arr) {
+//   console.log(value)
+// }
+
+// arr.forEach((v, i) => {
+//       console.log(v, i)
+// })
+
+//Copy or clone
+
+// var originalArr= ['sachin', { road: 101 }, () => { console.log('test') }]
+
+// var copiedArr = originalArr//normal copy
+// var copiedArr = JSON.parse(JSON.stringify(originalArr)) //deep copy
+// var copiedArr = Object.assign([], originalArr) //shallow copy (ES-5)
+// var copiedArr = originalArr.slice() //shallow copy (ES-5)
+// var copiedArr = [...originalArr]//shallow copy (ES-6)
+
+// copiedArr[0] = 'kohli'
+// copiedArr[1].road = 201
+
+// console.log(originalArr)
+// console.log(copiedArr)
+
+//testing of array items
+
+// var arr = [1, 2, 3, 4, 5]
+
+// var res = arr.some((item) => {
+//        return item > 1
+// })
+
+// console.log(res)
+
+// var res = arr.every((item) => {
+//        return item > 1
+// })
+
+// console.log(res)
+
+//How to convert array into string
+
+// var arr = [1, 2, 3, 4, 5]
+// console.log(arr, typeof arr)
+
+// var str = arr.join('+')
+
+// console.log(str, typeof str)
+
+// var myArr = str.split()
+// console.log(myArr, typeof myArr)
+
+// var arr = [1, 2, 3, 4, 5]
+
+// var arr = [{name: 'sachin', age: 40}, {name: 'dhoni', age: 35}, {name: 'kohli', age: 30}, {name: 'gill', age: 25}]
+
+// console.log(arr)
+
+// var resArr = arr.filter((obj) => {
+//            return  obj.age <=30
+// })
+
+// console.log(resArr)
+
+
+//How to  map array items with other value
+
+// var arr = [1, 2, 3, 4, 5]
+
+// console.log(arr)
+
+// var mapRes = arr.map((item) => {
+//       return  item * 100
+// })
+
+// console.log(mapRes)
+
+//Reduce method
+
+// var arr = [1, 2, 3, 4, 5]
+
+// var reduceRes = arr.reduce((cum, cur) => {
+//   console.log(cum, cur)
+//   return cum+cur
+// }, 0)
+
+// console.log(reduceRes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
