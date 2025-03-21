e611946e075e703c9f534ae2a8d04cd273dc9291
venkat ravilla <venkatravilla333@gmail.com>
2025-03-21 22:37:55 +0530

    content

---
+++ 9.Dom/index.js
@@ -0,0 +1,54 @@
+//what is DOM
+
+//How to examine DOM
+
+// console.dir(document)
+// console.log(document)
+
+//How to read DOM Properties (By using . operator)
+
+// console.log(document.URL)
+// console.log(document.doctype)
+// console.log(document.all)
+// console.log(document.forms)
+// console.log(document.links)
+
+//How to access DOM elements into JS FILE
+
+// ID
+
+// let para1 = document.getElementById('one')
+
+// console.log(para1)
+
+//class
+
+// let para1 = document.getElementsByClassName('myclass')
+// console.log(para1[0])
+// console.log(para1[1])
+// para1.innerText = 'hello'
+// para1.style.background = 'red'
+
+//Tag
+
+// var para = document.getElementsByTagName('p')
+// console.log(para)
+
+//querySelector
+
+// var para = document.querySelector('#one')
+// var para = document.querySelector('.myclass')
+// var para = document.querySelector('p')
+// console.log(para)
+
+//querySelectorAll
+
+// var para = document.querySelectorAll('#one')
+// var para = document.querySelectorAll('.myclass')
+// var para = document.querySelectorAll('p')
+// console.log(para)
+
+
+
+
+
